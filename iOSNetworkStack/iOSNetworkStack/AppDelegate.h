59be9fcd6583ad2d74bfbfbcdd978d2315cb872a
Zach McArtor <zmcartor@gmail.com>
2015-04-27 21:09:00 -0600

    proj file first commit

---
+++ iOSNetworkStack/iOSNetworkStack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSNetworkStack
+//
+//  Created by Zach McArtor on 4/27/15.
+//  Copyright (c) 2015 HackaZach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSNetworkStack/iOSNetworkStack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSNetworkStack
+//
+//  Created by Zach McArtor on 4/27/15.
+//  Copyright (c) 2015 HackaZach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
